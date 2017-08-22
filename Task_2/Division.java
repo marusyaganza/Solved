1df4915c4013f5acefa8976ab0787d033bcd2053
marusyaganza <marusyaganza@yandex.ru>
2017-08-22 21:30:13 +0400

    try utf-8

--- Task_2/Division.java
+++ Task_2/Division.java
@@ -1,9 +1,9 @@
-/**
+﻿/**
  * This class is designed to divide two numbers
  */
 public class Division {
     public static void main(String[] args) {
-        //If the number of arguments is less than 2
+        // Проверить число аргументов
         if(args.length<2) {
             System.out.println("two arguments needed");
             return;
@@ -14,16 +14,14 @@ public class Division {
                 b = Double.parseDouble(args[1]);
 
             } catch (Exception e) {
-		// If arguments are not numbers
                 System.out.println("Arguments must be numbers ");
                 return;
             }
-        // If denominator is 0
+        // Если знаменатель равен нулю
             if(b==0.0) {
                 System.out.println("Denominator can't be zero");
                 return;
             }
-            // Display the result
             System.out.println("Result is " + (a / b));
 
         }
